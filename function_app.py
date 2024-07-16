4606b69b7cbe0f834dc49f0dcb013b8d1d7182a5
thenameisajay <amahadeven@economicdatasciences.com>
2024-07-16 14:16:49 +0100

    added test user

--- function_app.py
+++ function_app.py
@@ -1,14 +1,19 @@
 import azure.functions as func
+
+# Routes Imported
 from blueprints.hello import bp as hello
 from blueprints.add_People import bp as add_people
 from blueprints.people import bp as people
 from blueprints.drop_people import bp as drop_people
+from blueprints.add_test_user import bp as add_test_user
 
- 
+# Set the auth level to anonymous for now ,
+# (https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook-trigger?tabs=python-v2
+# %2Cisolated-process%2Cnodejs-v4%2Cfunctionsv2&pivots=programming-language-python#authorization-keys)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
 
 app.register_blueprint(hello)
 app.register_blueprint(add_people)
 app.register_blueprint(people)
 app.register_blueprint(drop_people)
+app.register_blueprint(add_test_user)
